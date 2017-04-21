ce2a13aad2d4b1b779a2193d868fbfe43364b9b9
getsby <getsby@hotmail.com>
2017-04-20 22:07:44 -0400

    jQuery to add Div as grid

--- js/main.js
+++ js/main.js
@@ -1,55 +1,40 @@
-var sizeofpad = 500;
-var colour = "black" 
-var rainbow = false;
-var erase = false;
-var recentSize = 16 
 
-$(document).ready(function(){
-
-	addCells(recentSize)
-
-	$("#clear").click(function(){
-		$('#container').effect('shake',{times:2});
-		$("#DrawPad").empty();
-		addCells(recentSize);
-	});
-
-	$ ("#rainbow").click(function(){
-		rainbow = true;
-		erase = false;
-	});
-
-	$ ("#marker").click(function(){
-		rainbow = false;
-		erase = false;
-		colour = "black";
-
-	});
-	$ ("#eraser").click(function(){
-		rainbow = false;
-		erase = true;
-	});
-
-		
-});
 
 $(document).ready(function() {
-    for(var x = 0; x < 16; x++) {
-        for(var y = 0; y < 16; y++) {
-        	var unit = $("<div class='unit' style='position:absolute'></div>");
-            unit.appendTo('#container', {left:});
+    for(var x = 0; x < 41; x++) {
+        for(var y = 0; y < 41; y++) {
+            var unit = $("<div class='unit'></div>");
+            unit.appendTo('#DrawPad');
         }
     }
 });
 
+//var mouseDown = false;
+//$(document).mousedown(function(){
+//	mouseDown = true;}0.mouseup(function(){mouseDown = false});
+//})
 
-$(document).ready(function(){
-	$('.DrawPad').bind("mouseover",function(){
-		var colour = $(this).css("background-color");
+//  $('.unit').hover(function(){
+//      if (mouseDown == true){
+ //     	if (random == true){
+      //		colour1 = Math.Floor((Math.random()*255));
+      //		colour2 = Math.Floor((Math.random()*255));
+      	//	colour3 = Math.Floor((Math.random()*255));
+//
+      //		colour = "rgb("+ colour1 +"," + colour2 + "," + colour3 +")"
 
-		$(this).css("background", "#380606");
-		$(this).bind("mouseout",function(){
-			$(this).css("background", color)
-		})
-	})
-})
+ //     	}
+
+  //    	if (erase == true){
+  //    		colour == "white"
+  //    	}
+
+   //   	$(this).css("background-color",colour); 
+  //    }
+
+  //   };
+
+
+
+
+ //}
