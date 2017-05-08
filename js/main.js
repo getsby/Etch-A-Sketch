87747a5c67a01c9910b34fd07a5a2d66fa0f81cb
getsby <getsby@hotmail.com>
2017-05-07 21:58:36 -0400

    the working version

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-
-
+//------------add div grid------------
 $(document).ready(function() {
     for(var x = 0; x < 41; x++) {
         for(var y = 0; y < 41; y++) {
@@ -9,32 +8,52 @@ $(document).ready(function() {
     }
 });
 
-//var mouseDown = false;
-//$(document).mousedown(function(){
-//	mouseDown = true;}0.mouseup(function(){mouseDown = false});
-//})
+//---------draw with black marker----
+$(document).ready(function () {
+ var colorOrig=$(".unit").css('background-color');
+ $("#marker").click(function () {    
+    $(".unit").hover(function() {
+        //mouse down
+        $(this).css('background-color', 'black')  
+      },
+      function() {
+        //mouse up
+      $(this).css('background-color', 'black')
+  });
+  });
+  });
 
-//  $('.unit').hover(function(){
-//      if (mouseDown == true){
- //     	if (random == true){
-      //		colour1 = Math.Floor((Math.random()*255));
-      //		colour2 = Math.Floor((Math.random()*255));
-      	//	colour3 = Math.Floor((Math.random()*255));
-//
-      //		colour = "rgb("+ colour1 +"," + colour2 + "," + colour3 +")"
+ 
+ //------rainbow-------------
+ $(document).ready(function () {
+ $("#rainbow").click(function () {
+ $('.unit').hover(function() {
+   var randomColorChange = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+    $(this).css('background-color', randomColorChange);
+    });
+    });
+  });
 
- //     	}
+// --- to erase ----------
+$(document).ready(function () {
+ $("#eraser").click(function () {
+		$(".unit").hover(function() {
+        //mouse down
+        $(this).css('background-color', 'white')
+    },
+    function() {
+        //mouse up
+        $(this).css('background-color', 'white')
+  });
+  });
+ });
+ 
+//-----------clear the canvas--------
+ $(document).ready(function () {
+  $("#clear").click(function () {
+   //only the content inside of the element will be deleted
+   $(".unit").css('background-color', 'white')
+  });
+ });
 
-  //    	if (erase == true){
-  //    		colour == "white"
-  //    	}
 
-   //   	$(this).css("background-color",colour); 
-  //    }
-
-  //   };
-
-
-
-
- //}
